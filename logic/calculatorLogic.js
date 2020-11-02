bcef277d622b40e7001af73a178f8c2807b6ee89
Eric Zepeda <zepedaeric@gmail.com>
2020-11-01 23:10:29 -0800

    Set up basic HTML and basic DOM operations

---
+++ logic/calculatorLogic.js
@@ -0,0 +1,22 @@
+const numbers = document.getElementById('numbers');
+const resultDisplay = document.getElementById('result');
+const operators = document.getElementById('operations')
+
+
+const numbersToAdd = [];
+const operationToPerform = [];
+
+
+operators.addEventListener('click', event => {
+  if(event.target.className === "operator") {
+    console.log(event.target.innerHTML)
+    resultDisplay.innerHTML = event.target.innerHTML
+  }
+})
+
+numbers.addEventListener('click', event => {
+  if(event.target.className === "number") {
+    console.log(event.target.innerHTML)
+    resultDisplay.innerHTML += event.target.innerHTML
+  }
+})
