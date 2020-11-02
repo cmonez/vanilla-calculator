a871394b46446bac5cd3160d719a8f16bd95cf6b
Eric Zepeda <zepedaeric@gmail.com>
2020-11-01 23:35:56 -0800

    Work on logic for adding numbers into array

--- logic/calculatorLogic.js
+++ logic/calculatorLogic.js
@@ -3,8 +3,10 @@ const resultDisplay = document.getElementById('result');
 const operators = document.getElementById('operations');
 const calculate = document.getElementById('equals');
 
-const numbersToAdd = [];
-const operationToPerform = [];
+// Array to put all clicked numbbers
+let numberPlaceHolder = [];
+let numbersToCalculate = [];
+let operationToPerform = [];
 
 operators.addEventListener('click', (event) => {
   if (event.target.className === 'operator') {
@@ -13,15 +15,21 @@ operators.addEventListener('click', (event) => {
     } else {
       operationToPerform.push(event.target.innerHTML);
     }
-    console.log(operationToPerform);
+    // grab the LAST  number from numberPlaceHolder add to numbersToAdd
+    numbersToCalculate.push(numberPlaceHolder[numberPlaceHolder.length - 1]);
+    // reset numberPlaceHolder
+    numberPlaceHolder = [];
+    console.log(numbersToCalculate);
   }
 });
 
 numbers.addEventListener('click', (event) => {
   if (event.target.className === 'number') {
     console.log('HERE IS THE NUMBER CLICKED ATM', event.target.innerHTML);
+    // Keep adding digits to the html
     resultDisplay.innerHTML += event.target.innerHTML;
-    numbersToAdd.push(Number(resultDisplay.innerHTML));
-    console.log('AFTER CLICKING', numbersToAdd);
+    // push number
+    numberPlaceHolder.push(Number(resultDisplay.innerHTML));
+    console.log('AFTER CLICKING', numberPlaceHolder);
   }
 });
