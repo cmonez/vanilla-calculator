3b6d77807f3219f269a4ff5eeb30d5b6598f3aa1
Eric Zepeda <zepedaeric@gmail.com>
2020-11-01 23:25:12 -0800

    Push numbers and operators into arrays for reference

--- logic/calculatorLogic.js
+++ logic/calculatorLogic.js
@@ -1,23 +1,27 @@
 const numbers = document.getElementById('numbers');
 const resultDisplay = document.getElementById('result');
-const operators = document.getElementById('operations')
+const operators = document.getElementById('operations');
 const calculate = document.getElementById('equals');
 
 const numbersToAdd = [];
 const operationToPerform = [];
 
-
-operators.addEventListener('click', event => {
-  if(event.target.className === "operator") {
-    console.log(event.target.innerHTML)
-    resultDisplay.innerHTML = event.target.innerHTML
+operators.addEventListener('click', (event) => {
+  if (event.target.className === 'operator') {
+    if (event.target.innerHTML === '÷') {
+      operationToPerform.push('/');
+    } else {
+      operationToPerform.push(event.target.innerHTML);
+    }
+    console.log(operationToPerform);
   }
-})
+});
 
-numbers.addEventListener('click', event => {
-  if(event.target.className === "number") {
-    console.log(event.target.innerHTML)
-    resultDisplay.innerHTML += event.target.innerHTML
+numbers.addEventListener('click', (event) => {
+  if (event.target.className === 'number') {
+    console.log('HERE IS THE NUMBER CLICKED ATM', event.target.innerHTML);
+    resultDisplay.innerHTML += event.target.innerHTML;
+    numbersToAdd.push(Number(resultDisplay.innerHTML));
+    console.log('AFTER CLICKING', numbersToAdd);
   }
-})
-
+});
